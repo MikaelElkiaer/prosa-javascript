db39c6d220425fcd9a7bea84dcbf2c9c6a8b973f
mikaelec <mikaelec@gmail.com>
2016-02-10 16:41:05 +0100

    Added dynamic generation of pages.

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,9 @@
 var data, data2, head_array=[], body_array=[], footer_array=[];
 
+
 $(document).ready(() => {
+  genereatePages(17);
+
   $.ajax({
     url: 'json/content.json',
     success: contentCallback
@@ -29,4 +32,25 @@ function contentCallback (result) {
   });
   var startNumber = window.location.hash.slice(5);
   contentF(startNumber || 1);
+}
+
+function genereatePages(amount) {
+  var body = $('body');
+  for (var i = 1; i <= amount; i++) {
+    var div = $(`
+  <div data-role="page" id="page${i}">
+    <div data-role="header" data-position="fixed" class="header">
+      <h1 class="header"></h1>
+      <a href="#page${Math.max(1, i-1)}" class="ui-btn ui-btn-left ui-btn-icon-left ${i == 1 ? 'ui-disabled' : ''}">Forrige</a>
+      <a href="#page${Math.min(amount, i+1)}" class="ui-btn ui-btn-right ui-btn-icon-right ${i == amount ? 'ui-disabled' : ''}">Næste</a>
+    </div>
+    <div data-role="content" class="content">Content Uno</div>
+    <div data-role="footer" data-position="fixed">
+      <h4 class="footer"></h4>
+    </div>
+  </div>
+    `);
+    div.appendTo( $.mobile.pageContainer );
+    if (i == 1) $.mobile.changePage( div );
+  }
 }
